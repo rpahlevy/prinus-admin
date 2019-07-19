4db5d4bb86d886e830e56f3738beb78cf9887d50
rpahlevy <rio.pahlevy@gmail.com>
2019-07-19 18:35:42 +0700

    Menambahkan Auth -> masih 1 level

---
+++ src/Middlewares/UserMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Middlewares;
+
+use App\Controllers\Controller;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class UserMiddleware extends Controller
+{
+    public function __invoke(Request $request, Response $response, $next)
+    {
+        // before
+        $user_refresh_time = $this->session->user_refresh_time;
+        $now = time();
+
+        if (empty($user_refresh_time) || $user_refresh_time < $now) {
+            $this->session->destroy();
+            return $this->redirect($response, $this->route('login'), ['errors' => 'Silahkan login untuk melanjutkan']);
+        }
+        // before
+
+        $response = $next($request, $response);
+
+        // after
+        // after
+
+        return $response;
+    }
+}
